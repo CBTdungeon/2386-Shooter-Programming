3abf3dbddb4343f4a23be35b8a3b141f1341cb44
Jake Vu <jakevu3@gmail.com>
2022-02-04 05:28:16 -1200

    Updated calculation to negate air resistance +QOL improvement

--- Main.java
+++ Main.java
@@ -3,16 +3,21 @@ public class Main {
     public static void main(String[] args) {
 
         double Distance = 8.23;
+        System.out.println("limelight distance is: "+ Distance);
 
         double Height = 2.64;
+        System.out.println("limelight height is: "+ Height);
 
-        double sValue = Trajectory.sValue(Distance);
+        double theoDistance = Trajectory.theoDistance(Distance);
+        System.out.println("the distance to negate air resistance is: " + theoDistance);
+
+        double sValue = Trajectory.sValue(theoDistance);
         System.out.println("the S value is: "+ sValue);
 
-        double Angle = Trajectory.Angle(Distance, Height, sValue);
+        double Angle = Trajectory.Angle(theoDistance, Height, sValue);
         System.out.println("the Angle to shoot is: "+ Angle);
 
-        double Velocity = Trajectory.Velocity(8.23,Angle,2.64);
+        double Velocity = Trajectory.Velocity(theoDistance,Angle,Height);
         System.out.println("the velocity of the ball is: "+ Velocity);
     }
 }
--- Trajectory.java
+++ Trajectory.java
@@ -1,8 +1,15 @@
 public class Trajectory {
 
+    public static double theoDistance(double distance){
+        double c1 = Math.pow(distance,2)*0.0378;
+        double c2 = 1.15*distance + c1 ;
+        double theoDistance = c2 + 0.0397;
+        return theoDistance;
+    }
+
     public static double sValue(double distance) {
 
-        double sValue = 0.354*Math.pow(distance,2)+3.4*distance-88.8;
+        double sValue = (0.0679*Math.pow(distance,2))+(2.38*distance)-89.6;
         return sValue;
     }
 
@@ -11,15 +18,17 @@ public class Trajectory {
         double t1 = Math.tan(Math.toRadians(sValue))* distance - (2*height);
         double t2 = t1/(-1*distance);
         double t3 = Math.atan((t2));
-        double angle = Math.toDegrees(t3);
+        double angle = Math.toDegrees(t3);  
         return angle;
     }  
 
     public static double Velocity(double distance, double angle, double height) {
 
-        double t1 = -9.81*Math.pow(distance,2)*(1+Math.pow(Math.tan(Math.toRadians(angle)),2));
-        double t2 = t1/(2*height - 2* distance * Math.tan(Math.toRadians(angle)));
-        double Velocity = Math.sqrt(t2);
+        double t1 = -9.81*Math.pow(distance,2);
+        double t12 = (Math.pow(Math.tan(Math.toRadians(angle)),2)+ 1) * t1;
+        double t2 = (2*height - 2* distance * Math.tan(Math.toRadians(angle)));
+        double t22 = t12/t2;
+        double Velocity = Math.sqrt(t22);
         return Velocity;
     }
 
